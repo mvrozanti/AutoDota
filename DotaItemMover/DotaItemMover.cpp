4165b81459b9a209805922c598c151933fe6652b
mvrozanti <mvrozanti@hotmail.com>
2017-01-16 14:30:43 -0200

    First

--- DotaItemMover/DotaItemMover.cpp
+++ DotaItemMover/DotaItemMover.cpp
@@ -8,14 +8,13 @@
 #include<map>
 #include<vector>
 #include<fstream>
+#include<sstream>
 #pragma comment(lib, "user32.lib")
 using namespace std;
 /* Globals */
 int ScreenX = GetDeviceCaps(GetDC(0), HORZRES);
 int ScreenY = GetDeviceCaps(GetDC(0), VERTRES);
 vector<BYTE> ScreenData(3 * ScreenX*ScreenY);
-vector<BYTE> referenceImage;
-const char* referenceFile = "C:\\Users\\Nexor\\Desktop\\Untitled.png";
 bool isPicking = false;
 int* referenceDragPoint;
 void pick();
@@ -81,20 +80,30 @@ void printTuple(tuple<BYTE, BYTE, BYTE> t) {
 }
 
 
-int selected_hero = -1;
-int MONKEY = 0;
+int NONE = 0;
+int MONKEY = 1;
+int selected_hero = NONE;
 
 map<int, int*> HERO_POS = {
-	{ 0, new int[2] { 1112,415 } }
+	{ MONKEY, new int[2] { 1112,415 } }
 };
 
 void pickHero() {
-	int* pickPoint = new int[2]{ 1420, 794 };
+	int* pickPoint = new int[2]{ 1420, 794 };//the pick button
 	if (selected_hero == MONKEY) {
 		int* monkeyPoint = HERO_POS.at(MONKEY);
-		clickAt(monkeyPoint[0], monkeyPoint[1]);
-		Sleep(50);
-		clickAt(pickPoint[0], pickPoint[1]);
+		clickAt(monkeyPoint[0], monkeyPoint[1]);//select hero
+		Sleep(80);
+		clickAt(pickPoint[0], pickPoint[1]);//confirm
+		Sleep(80);
+	}
+}
+
+void beep(int times, int interval) {
+	for (size_t i = 0; i < times; i++)
+	{
+		cout << '\a';
+		Sleep(interval);
 	}
 }
 
@@ -119,24 +128,26 @@ void tryPickMid() {
 		cout << endl;
 		if (colorApprox(direColor, goColor)) {
 			clickAt(298, 943);
-			cout << '\a' << '\a' << '\a' << '\a' << '\a';
 			isPicking = false;
+			beep(2, 100);
 		}
 		else if (colorApprox(radiantColor, goColor)) {
 			clickAt(253, 965);
-			cout << '\a' << '\a' << '\a' << '\a' << '\a';
 			isPicking = false;
+			beep(2, 100);
 		}
 		else if (tries++ == 1000) {
 			isPicking = false;
+			beep(3, 200);
 		}
 	}
 	/*hero selection (it is assured that we are past
 	lane pick so we can choose hero)*/
+	Sleep(10);
 	pickHero();
 }
 
-bool isSlotPosSet = false;
+long double last_item_change_time = 0;
 map<int, int*> SLOT_POS = {
 	/*
 	OLD TIMES   (NOICE)
@@ -162,18 +173,13 @@ map<int, int*> SLOT_POS = {
 		 { 9, new int[2]{ 1326,977 } } < -RECENT SHITTY TIMES*/
 
 		 // << U P D A T E D >>
-		 { 4, new int[2]{   -9, -44} },
+		 { 4, new int[2]{   -2, -44} },
 		 { 5, new int[2]{  +62, -44} },
-		 { 6, new int[2]{ +125, -44} },
+		 { 6, new int[2]{ +145, -44} },
 		 { 7, new int[2]{   -2, -90} },
-		 { 8, new int[2]{  +62, -90} },
-		 { 9, new int[2]{ +125, -90} }   /* these are the offsets in relation to the tiny sword pointing to total physical damage*/
+		 { 8, new int[2]{  +70, -90} },
+		 { 9, new int[2]{ +145, -90} }   /* these are the offsets in relation to the tiny sword pointing to total physical damage*/
 };
-
-void mouseUp() {
-	thread t(mouse_event, MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-	t.join();
-}
 
 int from = -1, to = -1;
 void exec(int times) {
@@ -185,7 +191,7 @@ void exec(int times) {
 	Sleep(40);
 	mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
 	Sleep(30);
-	SetCursorPos((referenceDragPoint[0] + slotPos1[0] + slotPos2[0]) / 2, (referenceDragPoint[1] + slotPos1[1] + slotPos2[1]) / 2);
+	SetCursorPos((referenceDragPoint[0] * 2 + slotPos1[0] + slotPos2[0]) / 2, (referenceDragPoint[1] * 2 + slotPos1[1] + slotPos2[1]) / 2);
 	Sleep(50);
 	SetCursorPos(referenceDragPoint[0] + slotPos2[0], referenceDragPoint[1] + slotPos2[1]);
 	Sleep(30);
@@ -202,23 +208,21 @@ void findReferencePointForDrag() {
 		}
 	}
 	referenceDragPoint = new int[2]{ -1, -1 };
-	cout << '\a' << '\a' << '\a';
 }
 
 void addToStack(int slot) {
-	if (!isSlotPosSet) {
+	if (time(0) - last_item_change_time > 60 * 3/*3 mins*/) {
 		findReferencePointForDrag();
 		cout << "Reference Drag Point:" << referenceDragPoint[0] << ',' << referenceDragPoint[1] << endl;
-		isSlotPosSet = true;
 	}
 	cout << "added to stack: " + slot << endl;
 	if (from == -1 && to == -1) {
 		from = slot;
-		cout << "from=" + from << endl;;
+		cout << "from=" << from << endl;;
 	}
 	else if (from != -1 && to == -1) {
 		to = slot;
-		cout << "from=" + from << endl;;
+		cout << "to=" << to << endl;;
 		if (from != to) {
 			exec(2);
 		}
@@ -231,18 +235,29 @@ void addToStack(int slot) {
 	}
 }
 
+void selectHero() {
+	cin.sync();
+	cin.clear();
+	cout << endl << "Input hero number:" << endl;
+	cout << "0 - None" << endl;
+	cout << "1 - Monkey King" << endl;
+	char a;
+	cin >> a;
+	selected_hero = a - '0';
+}
+
 HHOOK hHook;
 thread t;
 void pick() {
 	if (!t.joinable()) {
 		isPicking = true;
+		cout << '\a';
 		t = thread(tryPickMid);
 	}
 	else {
-		::TerminateThread(t.native_handle(), 0);
-		t.detach();
 		isPicking = false;
-		pick();
+		t.join();
+		pick();//idk why
 	}
 }
 
@@ -250,8 +265,7 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
 	if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN)
 	{
 		PKBDLLHOOKSTRUCT pKey = (PKBDLLHOOKSTRUCT)lParam;
-		cout << pKey->scanCode << "(";
-		cout << pKey->vkCode << ") ";
+		cout << pKey->vkCode << " ";
 		switch (pKey->vkCode) {
 		case 97://1
 			addToStack(1);
@@ -293,8 +307,7 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
 			pick();
 			break;
 		case 111:///
-			selected_hero = MONKEY;
-			cout << "MONKEY selected" << endl;
+			selectHero();
 			break;
 		}
 	}
@@ -313,12 +326,11 @@ void startLogging() {
 
 int main() {
 	cout << "Runnning..." << endl;
-	if ((GetKeyState(VK_NUMLOCK) & 0x0001) == 0) {
-		keybd_event(VK_NUMLOCK, 0x45, KEYEVENTF_EXTENDEDKEY, 0);
+	if ((GetKeyState(VK_NUMLOCK) & 0x0001) == 0) {//if numlock not enabled
+		keybd_event(VK_NUMLOCK, 0x45, KEYEVENTF_EXTENDEDKEY, 0);//enable
 		keybd_event(VK_NUMLOCK, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
 	}
-	thread t = thread(startLogging);
-	t.join();
+	startLogging();
 	return 0;
 }
 
